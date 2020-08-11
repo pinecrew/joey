f4eb95a69042dfb447a4aeb709b348a6089b4872
Alexey Golubev <dr.freecx@gmail.com>
2020-08-11 17:55:53 +0400

    update function args

--- manage.py
+++ manage.py
@@ -2,20 +2,21 @@ from pathlib import Path
 import argparse
 
 
-def app_init():
-    print('app:init')
+def app_init(name):
+    app_directory = Path(name) if name else Path('.')
+    print(f'app:init in <{app_directory}> directory')
 
 
-def app_add(args):
-    print(f'app:add {args}')
+def app_add(name):
+    print(f'app:add application with name <{name}>')
 
 
 def app_run():
     print('app:run')
 
 
-def app_revise(args):
-    print(f'app:revise {args}')
+def app_revise(label):
+    print(f'app:revise with label <{label}>')
 
 
 def app_migrate():
@@ -25,7 +26,7 @@ def app_migrate():
 if __name__ == '__main__':
     # supported functions
     params = [
-        ('init', 'create project skeleton', False, app_init),
+        ('init', 'create project skeleton', True, app_init),
         ('add', 'add project application', True, app_add),
         ('run', 'run developer server', False, app_run),
         ('revise', 'create migration', True, app_revise),
@@ -39,14 +40,15 @@ if __name__ == '__main__':
     for name, help_info, has_args, function in params:
         s_parse = subparser.add_parser(name, help=help_info)
         if has_args:
-            s_parse.add_argument(dest='args', type=str, default='')
+            s_parse.add_argument(dest='argument', nargs='?', type=str, default='', help='command parameter')
         s_parse.set_defaults(func=function)
 
     option = parser.parse_args()
     # if args has func, then execute this function
     if hasattr(option, 'func'):
-        if hasattr(option, 'args'):
-            option.func(option.args)
+        # with arguments?
+        if hasattr(option, 'argument'):
+            option.func(option.argument)
         else:
             option.func()
     else:
