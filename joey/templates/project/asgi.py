af402cbd6eea554954938d7adc26812c546ceb0c
Vova Abdrakhmanov <369565@gmail.com>
2020-08-14 18:21:14 +0000

    First try

--- joey/templates/project/asgi.py
+++ joey/templates/project/asgi.py
@@ -5,13 +5,18 @@ from fastapi import FastAPI
 from joey import db
 from settings import settings
 
+
 db.init(settings)
 
 
 app = FastAPI()
-for application in settings.APPLICATIONS:
-    try:
-        m = import_module(application + '.routes')
-        app.include_router(m.router, prefix='/' + application, tags=[application])
-    except:
-        pass
+
+
+def router(module_name):
+    return import_module(module_name).router
+
+
+# Specify routers below
+
+# for example:
+# app.include_router(router('application.routes'), prefix='/application' , tags=['application'])
