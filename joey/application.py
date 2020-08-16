3d6568c22de69b3dfaf5fae1e834ce5d5fbdbf12
Vova Abdrakhmanov <369565@gmail.com>
2020-08-16 09:42:58 +0000

    Fix error with items in routes iteration

--- joey/application.py
+++ joey/application.py
@@ -6,5 +6,5 @@ class Application(FastAPI):
     def __init__(self, settings, *args, **kwargs):
         super().__init__(*args, **kwargs)
         db.init(settings)
-        for application_name, properties in settings.ROUTES:
+        for application_name, properties in settings.ROUTES.items():
             self.include_router(utils.get_router(application_name), **properties)
