2775b9041b7434fb7de611e87a41f13d41766b26
Vova Abdrakhmanov <369565@gmail.com>
2022-04-10 23:00:32 +0300

    fix: tests to support current joey.db

--- tests/test_auto_id.py
+++ tests/test_auto_id.py
@@ -14,14 +14,10 @@ db_context = db.init(settings)
 
 @pytest.fixture(autouse=True, scope="module")
 def create_test_database():
-    engine = db_context.engine
-    metadata = db_context.metadata
-    metadata.create_all(engine)
+    async_adapter(db_context.models.create_all)()
     yield
-    metadata.drop_all(engine)
+    async_adapter(db_context.models.drop_all)()
 
-class Example(db.Model):
-    description = orm.Text(allow_blank=True)
 
 def async_adapter(wrapped_func):
     """
@@ -30,17 +26,40 @@ def async_adapter(wrapped_func):
 
     @functools.wraps(wrapped_func)
     def run_sync(*args, **kwargs):
-        loop = asyncio.get_event_loop()
         task = wrapped_func(*args, **kwargs)
-        return loop.run_until_complete(task)
+        return asyncio.run(task)
 
     return run_sync
 
+class Example(db.Model):
+    fields = {
+        "id": orm.Integer(primary_key=True),
+        "description": orm.Text(allow_blank=True),
+    }
 
 @async_adapter
-async def test_auti_id():
+async def test_example():
     await Example.objects.create()
 
     example = await Example.objects.get()
+    assert example.id == 1
+    assert example.description == ''
+
+class Parent(db.Model):
+    abstract = True
+    fields = {
+        "id": orm.Integer(primary_key=True),
+    }
+
+class Child(Parent):
+    fields = {
+        "description": orm.Text(allow_blank=True),
+    }
+
+@async_adapter
+async def test_abstract():
+    await Child.objects.create()
+
+    example = await Child.objects.get()
     assert example.id == 1
     assert example.description == ''
